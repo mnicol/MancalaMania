af1bed7274b5f98a168d2c3a3bc12e1eec3865f9
nicol <mnicol@wisc.edu>
2016-02-19 08:43:31 -0600

    Added comand line arguments for number of randos and formated the output some.

--- src/smartTest.java
+++ src/smartTest.java
@@ -11,20 +11,25 @@ public class smartTest {
 	 */
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
-		PrintStream out;
+		/*PrintStream out;
 		try {
 			out = new PrintStream(new FileOutputStream(startTime + ".txt"));
 			System.setOut(out);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		int numRandos = 1000;
+			}*/
+
+		int numRandos = 0;
+		if(args.length == 1)
+		    numRandos = Integer.parseInt(args[0]);
+		else
+		    numRandos = 1000;
+
 		Colosseum colosseum = new Colosseum(7, numRandos, 0, null);
 		ArrayList<StandardFighters> bestFighters = colosseum.BrawlAndGetMostBadAssMother();
 		for (int i = 0; i < bestFighters.size(); i ++){
+		    System.out.println("Number of Players: " + numRandos);
 			System.out.print("Wins: " + bestFighters.get(i).GamesWon + " Losses:" + bestFighters.get(i).GamesLost + " ");
 			for (int j =0; j < TestSBE.NumWeights; j ++){
 				System.out.print(bestFighters.get(i).Weights[j] );
@@ -32,11 +37,12 @@ public class smartTest {
 					System.out.print(", ");
 				}
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
 		long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		System.out.println(totalTime);
+		System.out.println("Total Time(ms): " + totalTime);
+		System.out.println();
 	}
 
 }
